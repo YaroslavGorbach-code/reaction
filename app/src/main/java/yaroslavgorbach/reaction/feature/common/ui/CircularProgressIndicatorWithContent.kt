279fddeb40ccb9f54a099584e6070ea44c9ff52c
YaroslavGorbach-code <yaroslavgorbach2@gmail.com>
2022-01-03 14:15:01 +0200

    Improve exercises list item ui

---
+++ app/src/main/java/yaroslavgorbach/reaction/feature/common/ui/CircularProgressIndicatorWithContent.kt
@@ -0,0 +1,39 @@
+package yaroslavgorbach.reaction.feature.common.ui
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxScope
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun CircularProgressIndicatorWithContent(
+    modifier: Modifier,
+    progress: Float,
+    strokeColor: Color = Color.Green,
+    backgroundStrokeColor: Color = Color.LightGray,
+    strokeWidth: Dp = 1.dp,
+    backgroundStrokeWidth: Dp = 1.dp,
+    content: @Composable BoxScope.() -> Unit,
+) {
+    Box(modifier = modifier) {
+        CircularProgressIndicator(
+            progress = progress,
+            color = strokeColor,
+            strokeWidth = strokeWidth,
+            modifier = Modifier.drawBehind {
+                drawCircle(
+                    color = backgroundStrokeColor,
+                    radius = (size.maxDimension / 2.0f) - (strokeWidth.value),
+                    style = Stroke(width = backgroundStrokeWidth.toPx())
+                )
+            }
+        )
+        content()
+    }
+}
--- app/src/main/java/yaroslavgorbach/reaction/feature/listexercises/ui/Exercises.kt
+++ app/src/main/java/yaroslavgorbach/reaction/feature/listexercises/ui/Exercises.kt
@@ -2,21 +2,22 @@ package yaroslavgorbach.reaction.feature.listexercises.ui
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Settings
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
-import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Alignment.Companion.End
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import yaroslavgorbach.reaction.common.ui.theme.ReactionTheme
 import yaroslavgorbach.reaction.data.listExercises.local.model.Exercise
@@ -78,16 +79,23 @@ internal fun Exercises(
                 .padding(end = 8.dp)
                 .clickable { actioner(ExercisesActions.OpenSettings) })
 
-        TrainingSurface(
-            trainings = state.trainings,
-            onTraining = { actioner(ExercisesActions.OpenTraining) },
-            modifier = Modifier
-                .fillMaxWidth()
-                .align(CenterHorizontally)
-                .padding(start = 8.dp, end = 8.dp, top = 8.dp)
-        )
-
         LazyColumn {
+             item {
+                 Text(
+                     text = "Упражнения",
+                     style = MaterialTheme.typography.caption,
+                     fontSize = 30.sp,
+                     modifier = Modifier.padding(start = 8.dp)
+                 )
+
+                 Text(
+                     text = "Практикуйтесь ежидневно и результат не заставит себя долго ждать",
+                     style = MaterialTheme.typography.subtitle1,
+                     fontSize = 16.sp,
+                     modifier = Modifier.padding(start = 8.dp)
+                 )
+
+             }
             items(state.exercises) { exercise ->
                 ExerciseItem(exercise = exercise) {
                     actioner(ExercisesActions.OpenDetails(exerciseName = exercise.name))

91e24190ee0c0417bbdc474f9d79f0b60b88337b
YaroslavGorbach-code <yaroslavgorbach2@gmail.com>
2022-01-30 12:25:06 +0200

    Fix incorrect exercise description

--- app/src/main/java/yaroslavgorbach/reaction/feature/exercise/common/mapper/ExerciseNameToDescriptionResMapper.kt
+++ app/src/main/java/yaroslavgorbach/reaction/feature/exercise/common/mapper/ExerciseNameToDescriptionResMapper.kt
@@ -9,7 +9,7 @@ object ExerciseNameToDescriptionResMapper {
             ExerciseName.EXTRA_NUMBER -> R.string.description_extra_number
             ExerciseName.EXTRA_WORD -> R.string.description_extra_word
             ExerciseName.FACE_CONTROL -> R.string.description_face_control
-            ExerciseName.COMPLEX_SORT -> R.string.description_face_control
+            ExerciseName.COMPLEX_SORT -> R.string.description_complex_sort
             ExerciseName.NO_NAME -> 0
             ExerciseName.STROOP -> R.string.description_stroop
             ExerciseName.GEO_SWITCHING -> R.string.description_geo_switching
